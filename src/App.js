129e3c1351aae2dde30e2c2ac76f6fef0cc37373
Berislav <berislav.hunjadi@gmail.com>
2021-03-25 11:30:42 +0100

    Added mathjs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+console.log('APP', Date.now());
+
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import {evaluate} from 'mathjs';
+
+console.log('index.js', Date.now());
 
 ReactDOM.render(
   <React.StrictMode>
